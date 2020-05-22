585b47e3ab20144fdd19a15eaf6499ef812ec965
Lettbee <lettbee@126.com>
2020-05-22 22:26:55 +0800

    ..

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,3 @@
 <?php
 use Illuminate\Support\Facades\Route;
 Route::auto('dev', 'DevController');
-Route::auto('keno-de/open', 'KenoDe\OpenController');
-Route::auto('keno-de/web', 'KenoDe\WebController');
-Route::get('/lotto/{lotto_name}/opened-log', 'LottoController@openedLog');
-Route::get('/bitcoin-collect', 'LottoController@bitcoinCollect');
