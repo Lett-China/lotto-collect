0bbc58dc24c60e34d0ba510457e1a44371389db3
Lettbee <lettbee@126.com>
2020-06-11 04:34:53 +0800

    走势图 bug fix.

--- app/Http/Controllers/LottoChartController.php
+++ app/Http/Controllers/LottoChartController.php
@@ -15,6 +15,9 @@ class LottoChartController extends Controller
             'ca28'  => '加拿大',
             'cw28'  => '加拿大西部',
             'bit28' => '比特币',
+            'de28'  => '德国',
+            'bj28'  => '北京',
+            'pc28'  => '蛋蛋',
         ];
 
         $chart = [
@@ -27,7 +30,7 @@ class LottoChartController extends Controller
         $limit = request()->limit ?: 100;
 
         $last       = LottoUtils::model($request->name)->where('status', '2')->orderBy('id', 'desc')->first();
-        $cache_name = 'lottoTrendChart.' . $request->name . ':' . $last->id;
+        $cache_name = 'lottoTrendChart.' . $request->name . ':' . $last->id . '--' . $limit;
 
         $items = cache()->remember($cache_name, 86400, function () use ($request) {
             $model = new LottoChart();
