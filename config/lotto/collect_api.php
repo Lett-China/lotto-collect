a48e3b024daa77aa2cb7d3e7b2e021a9fcc901cd
Lettbee <lettbee@126.com>
2020-07-07 15:23:34 +0800

    完善 星彩 采集。

--- config/lotto/collect_api.php
+++ config/lotto/collect_api.php
@@ -1,26 +1,22 @@
 <?php
-
-$open_cai = [];
-
-$data_be = [
-    'bjkl8'   => 'bj28',
-    'cabskl8' => 'ca28',
-    'bjpk10'  => 'pk10',
-    'hubk3'   => 'hubk3',
-    'hebk3'   => 'hebk3',
-    'cqssc'   => 'cqssc',
-    'ahk3'    => 'ahk3',
-    'tjssc'   => 'tjssc',
-    'shk3'    => 'shk3',
-    'jsk3'    => 'jsk3',
-    'xjssc'   => 'xjssc',
-    'hljssc'  => 'hljssc',
+$xing_cai = [
+    'bjhl8'  => 'bj28',
+    'jndhl8' => 'ca28',
+    'bjpk10' => 'pk10',
+    'hbk3'   => 'hubk3',
+    'hebk3'  => 'hebk3',
+    'cqssc'  => 'cqssc',
+    'ahk3'   => 'ahk3',
+    'tjssc'  => 'tjssc',
+    'shk3'   => 'shk3',
+    'jsk3'   => 'jsk3',
+    'xjssc'  => 'xjssc',
+    'hljssc' => 'hljssc',
+    'bjk3'   => 'bjk3',
 ];
 
 $result = [
-    'open_cai' => $open_cai,
-    'data_be'  => $data_be,
-    'xing_cai' => $data_be,
+    'xing_cai' => $xing_cai,
 ];
 
 return $result;
