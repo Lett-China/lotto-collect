62de01728293046baa663b1c7a72c7d9c90e7539
Lettbee <lettbee@126.com>
2020-09-28 13:35:23 +0800

    修改比特币采集

--- app/Models/LottoModule/Models/LottoBit28.php
+++ app/Models/LottoModule/Models/LottoBit28.php
@@ -190,7 +190,7 @@ class LottoBit28 extends BasicModel
 
         //根据下注额控制
         $control_val = $control->formulaBet($lotto_index, $open_code, $this->lotto_name);
-        if ($count <= 10) {
+        if ($count <= 30) {
             if (($item->control === 'bet' && $control_val > 0) || $control_val >= 1000) {
                 $count += 1;
                 goto start;
