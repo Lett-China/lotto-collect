696ff084529286e8ae60d742600b85705098be29
Lettbee <lettbee@126.com>
2021-01-04 20:49:12 +0800

    。

--- config/lotto/base.php
+++ config/lotto/base.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'collect_own'      => ['de28', 'in28', 'kojc28', 'kolt28', 'ndlt28'], //自有游戏
+    'collect_own'      => ['de28', 'in28', 'kojc28', 'kolt28', 'ndlt28', 'lult28'], //自有游戏
     'formula_basic28'  => ['bj28', 'ca28', 'de28', 'hero28', 'cw28', 'in28', 'kojc28', 'kolt28', 'ndlt28', 'lult28', 'tw28'], //用基本算法的28
     'win_place_has_st' => ['ca28', 'cw28', 'de28', 'bj28', 'in28', 'kojc28', 'kolt28', 'ndlt28', 'lult28', 'tw28'], //有16的游戏
 ];
