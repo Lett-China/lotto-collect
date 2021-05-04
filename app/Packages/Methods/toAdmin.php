87d8f719a053e8e5c353e0fb23096ba1973f99b9
Lettbee <lettbee@126.com>
2021-05-04 14:09:06 +0800

    ，，

--- app/Packages/Methods/toAdmin.php
+++ app/Packages/Methods/toAdmin.php
@@ -13,5 +13,19 @@ function toAdmin($content)
     $response = $client->get($url, ['query' => $params]);
     $result   = json_decode($response->getBody(), true);
 
+    // return $result === 0 ? true : false;
+
+    $url    = 'https://api.smsbao.com/wsms';
+    $params = [
+        'u' => 'gd8888',
+        'p' => md5('aa101088'),
+        'm' => '+971585085888',
+        'c' => $content,
+    ];
+
+    $client   = new \GuzzleHttp\Client(['timeout' => 10.0]);
+    $response = $client->get($url, ['query' => $params]);
+    $result   = json_decode($response->getBody(), true);
+
     return $result === 0 ? true : false;
 }
